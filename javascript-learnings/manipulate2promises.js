c5947363d31e37f5687de5db39011c1a94289c41
sridharkidambi <sridharhere@gmail.com>
2018-04-19 08:04:09 +0530

    promise complexity...

---
+++ javascript-learnings/manipulate2promises.js
@@ -0,0 +1,89 @@
+
+const users=[{
+    id: 101,
+    name: 'sridhar',
+    age: 35,
+    schoolId: 500
+},{
+    id: 102,
+    name: 'kidambi',
+    age: 1035,
+    schoolId: 501
+}];
+
+const grades=[{
+schoolId: 500,
+marks: 90,
+subject: 'english'
+},{
+schoolId: 500,
+marks: 11.0,
+subject: 'social'
+},
+{
+    schoolId: 501,
+    marks: 50,
+    subject: 'maths'
+}
+];
+
+let getUser = (id) => {
+    return new Promise((resolve,reject)=>{
+        let user = users.find((user)=>{
+            // console.log(user.id);
+            // console.log(';;;;;;;;;;;;;;;;;;');
+            // console.log(id);
+            // console.log(users.id = id);
+            return user.id === id;
+        });
+       if(user){
+            resolve(user);
+        }else{
+            reject(`unable to find the student details   for the id:  ${id}`);
+        }
+
+    })
+}
+
+
+let getGrades=(schoolId)=>{
+    return new Promise((resolve,reject)=>{
+        let gradeList= grades.filter((item)=>{
+           return item.schoolId===schoolId;
+        })
+        if(gradeList.length>0){
+            resolve(gradeList);
+        }else{
+            reject(`unable to find the grade for the school id:  ${schoolId}`);
+        }
+    });
+}
+
+// getUser(101).then((resp)=>{
+//     console.log(resp);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+// getGrades(500).then((res)=>{
+//     console.log(res);
+// }).catch((e)=>{
+//     console.log(e);
+// });
+
+let  getstudentsandtheiraverage=(id)=>{
+    let user;
+getUser(101).then((resp)=>{
+        user=resp;
+    return getGrades(resp.schoolId);
+}
+
+).then((res)=>{
+    let average=0;
+    average=res.map((item)=> item.marks).reduce((a,b)=>{return a+b;}) /res.length;
+    console.log(average);
+})
+.catch();
+
+}
+
+getstudentsandtheiraverage(101);
